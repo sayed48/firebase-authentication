f2f479534a591556b119a009b2b1221e38d87547
sayed48 <masayedmas48@gmail.com>
2022-04-08 23:24:41 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import app from "./firebase.init";
+import { Button, Form } from "react-bootstrap";
+import { useState } from "react";
+
+const auth = getAuth(app);
+
+function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+
+  const handleEmailBlur = event => {
+    setEmail(event.target.value);
+  };
+  const handlePasswordBlur = event => {
+    setPassword(event.target.value);
+  };
+  const handleFormSubmit = event => {
+    createUserWithEmailAndPassword(auth, email, password)
+    .then(result => {
+      const user = result.user;
+      console.log(user);
+    })
+    .catch(error =>{
+      console.error(error);
+    })
+    event.preventDefault();
+  };
+
+  return (
+    <div>
+      <div className="registration w-50 mx-auto mt-5">
+        <h1 className="text-primary">Please Register!</h1>
+        <Form onSubmit={handleFormSubmit}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group> 
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Password" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyASwxDxA3M5arZ7-jzGPai1cX83XNu1YBI",
+  authDomain: "fir-authentication-27278.firebaseapp.com",
+  projectId: "fir-authentication-27278",
+  storageBucket: "fir-authentication-27278.appspot.com",
+  messagingSenderId: "345154129638",
+  appId: "1:345154129638:web:9550f6b7e332df5cd2f527"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
